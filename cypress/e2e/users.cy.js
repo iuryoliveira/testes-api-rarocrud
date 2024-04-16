d5be2d404556daf15b22c6c703e60bd679b6a3d7
Iury Oliveira <iuryoliveirasx@gmail.com>
2024-04-16 19:56:37 -0300

    testes de bad requests de /users

--- cypress/e2e/users.cy.js
+++ cypress/e2e/users.cy.js
@@ -1,5 +1,43 @@
-describe('Testes da rota /users', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io');
+describe('Testes da rota /users', function () {
+  describe('Testes de Bad requests', function () {
+    it('Deve receber bad request ao tentar cadastrar um usuário sem e-mail', function () {
+      cy.request({
+        method: 'POST',
+        url: '/users',
+        body: {
+          name: 'iury oliveira',
+        },
+        failOnStatusCode: false,
+      })
+        .its('status')
+        .should('to.equal', 400);
+    });
+
+    it('Deve receber bad request ao tentar cadastrar um usuário sem o nome', function () {
+      cy.request({
+        method: 'POST',
+        url: '/users',
+        body: {
+          email: 'i@t.com',
+        },
+        failOnStatusCode: false,
+      })
+        .its('status')
+        .should('to.equal', 400);
+    });
+
+    it('Deve receber bad request ao tentar cadastrar um usuário com e-mail inválido', function () {
+      cy.request({
+        method: 'POST',
+        url: '/users',
+        body: {
+          name: 'iury oliveira',
+          email: '.com',
+        },
+        failOnStatusCode: false,
+      })
+        .its('status')
+        .should('to.equal', 400);
+    });
   });
 });
