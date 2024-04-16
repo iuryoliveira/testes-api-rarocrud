d55afd62747deaea7367ae16ea793927c0781c6f
Iury Oliveira <iuryoliveirasx@gmail.com>
2024-04-16 19:37:14 -0300

    nome do describe

--- cypress/e2e/users.cy.js
+++ cypress/e2e/users.cy.js
@@ -1,5 +1,5 @@
-describe('template spec', () => {
+describe('Testes da rota /users', () => {
   it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+    cy.visit('https://example.cypress.io');
+  });
+});
