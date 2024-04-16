03098806139f42059781191d0c349370bc2ceba3
Iury Oliveira <iuryoliveirasx@gmail.com>
2024-04-16 19:28:07 -0300

    configura baseUrl

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,8 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: 'https://rarocrud-80bf38b38f1f.herokuapp.com/api/v1',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
