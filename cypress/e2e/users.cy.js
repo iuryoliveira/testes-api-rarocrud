b26a06dc1bc59a8549b96a3a98da08c68a054ca2
Iury Oliveira <iuryoliveirasx@gmail.com>
2024-04-16 21:09:53 -0300

    implementacao testes de bad request e criacao de usuario

--- cypress/e2e/users.cy.js
+++ cypress/e2e/users.cy.js
@@ -1,3 +1,5 @@
+import { faker } from '@faker-js/faker';
+
 describe('Testes da rota /users', function () {
   describe('Testes de Bad requests', function () {
     it('Deve receber bad request ao tentar cadastrar um usuário sem e-mail', function () {
@@ -8,9 +10,10 @@ describe('Testes da rota /users', function () {
           name: 'iury oliveira',
         },
         failOnStatusCode: false,
-      })
-        .its('status')
-        .should('to.equal', 400);
+      }).then((response) => {
+        expect(response.status).to.equal(400);
+        expect(response.body).to.be.undefined;
+      });
     });
 
     it('Deve receber bad request ao tentar cadastrar um usuário sem o nome', function () {
@@ -38,6 +41,57 @@ describe('Testes da rota /users', function () {
       })
         .its('status')
         .should('to.equal', 400);
+    });
+  });
+
+  describe.only('Testes de criação de usuário', function () {
+    var idUsuario;
+
+    afterEach(function () {
+      cy.deletarUsuario(idUsuario);
+    });
+
+    it('Deve ser possível criar usuário com dados válidos', function () {
+      const name = 'iury oliveira';
+      const email = 'iuryasgdyas@sdausdhaus.com';
+      cy.request('POST', '/users', {
+        name: name,
+        email: email,
+      }).then(function (response) {
+        expect(response.status).to.equal(201);
+        expect(response.body).to.have.property('id');
+        expect(response.body).to.have.property('createdAt');
+        expect(response.body).to.have.property('updatedAt');
+        expect(response.body.name).to.equal(name);
+        expect(response.body.email).to.equal(email);
+
+        idUsuario = response.body.id;
+      });
+    });
+
+    it('Não deve ser possível cadastrar usuário com e-mail já utilizado', function () {
+      const name = faker.person.fullName();
+      const email = faker.internet.email();
+
+      cy.request('POST', '/users', {
+        name: name,
+        email: email,
+      }).then((response) => {
+        idUsuario = response.body.id;
+
+        cy.request({
+          method: 'POST',
+          url: '/users',
+          body: {
+            name: name,
+            email: email,
+          },
+          failOnStatusCode: false,
+        }).then((response) => {
+          expect(response.status).to.equal(422);
+          expect(response.body.error).to.equal('User already exists.');
+        });
+      });
     });
   });
 });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,7 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('deletarUsuario', function (id) {
+  cy.request('DELETE', 'users/' + id);
+});
